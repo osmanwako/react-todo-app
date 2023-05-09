b982ef9dea6ccbb9474ec30ec021613c6df200f6
OsmanFP <osmanwako411@gmail.com>
2023-05-09 23:31:40 +0300

    Delete todo from  list

--- src/component/todoapp.js
+++ src/component/todoapp.js
@@ -1,51 +1,52 @@
+import { v4 as uuidv4 } from "uuid";
 import Todotitle from './todotitle';
 import Todoform from './todoform';
 import Todolist from './todolist';
 import { useState } from 'react';
 
 function TodoApp() {
+  const [message, setmessage] = useState('');
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Gather project requirements',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Feasibility study and Analysis',
       completed: true,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 4,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 5,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
   ]);
-  const [warn, setmessage] = useState(false);
   const addTodo = (title) => {
-    if (title === '') {
-      setmessage(true);
+    if (title.trim()) {
+      setTodos(() => [...todos, { id: uuidv4(), title: title, completed: false }]);
+      setmessage('');
     }
     else {
-      setTodos(() => [...todos, { id: todos.length, title: title, completed: false }]);
-      setmessage(false);
+      setmessage('Please insert Your todo!');
     }
   }
   return (
     <div class="todos">
       <Todotitle />
       <Todoform addItem={addTodo} />
-      <span className={warn ? 'submit-warning' : 'd-none'} >Please insert your todo list</span>
+      <span className="submit-warning" >{message}</span>
       <Todolist todosProps={todos} setTodos={setTodos} />
     </div>
   );
--- src/component/todoitem.js
+++ src/component/todoitem.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
-function Todoitem({ itemProp, setTodos }) {
+function Todoitem({ itemProp, setTodos, removeId }) {
     const [check, setCheck] = useState(itemProp.completed);
     const oncheck = () => {
         setCheck(() => !check);
     }
+
     return <li className="to-do-list">
         <span>
             <input type="checkbox" className="input-check" checked={check} onClick={oncheck} />
             <span className={check ? 'todo-complete' : 'todo-start'}> {itemProp.title}</span>
         </span>
-        <button>Delete</button>
+        <button onClick={() => removeId(itemProp.id)}>Delete</button>
     </li>;
 }
 
