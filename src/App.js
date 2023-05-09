2766dd3ecf6bec84d01ddb47374de0900cc072a2
OsmanFP <osmanwako411@gmail.com>
2023-05-09 15:18:55 +0300

    TodoApp functional component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
 import './App.css';
-
+import TodoApp from './component/todoapp';
 function App() {
   return (
-    <div class="todos">
-      <header class="todo-header">
-        <h1>To do Applications</h1>
-        <p>Items will persist in the browser local storage</p>
-      </header>
-      <form class="form-container">
-        <input
-          type="text"
-          placeholder="Add Todo..."
-          class="input-text input-both"
-          value=""
-        />
-        <button class="input-submit">&#10010;</button>
-      </form>
-      <span class="submit-warning">Please insert your todo list</span>
-      <ul>
-        <li className="to-do-list">
-          <span>
-            <input type="checkbox" className="input-check" />
-            <span className="todo-selected">Osman Wako</span>
-          </span>
-          <button>Delete</button>
-        </li>
-        <li className="to-do-list">
-          <span>
-            <input type="checkbox" className="input-check" />
-            <span>Osman Wako</span>
-          </span>
-          <button>Delete</button>
-        </li>
-        <li className="to-do-list">
-          <span>
-            <input type="checkbox" className="input-check" />
-            <span>Osman Wako</span>
-          </span>
-          <button>Delete</button>
-        </li>
-      </ul>
+    <div class="wrapper">
+      <TodoApp />
     </div>
   );
-  
 }
 
 export default App;
