05fcdc9dc89b2a9f3eb35f90e01b6d63efb2602c
OsmanFP <osmanwako411@gmail.com>
2023-05-09 14:38:38 +0300

    Create initial js and css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import './App.css';
 
 function App() {
-  return <div className="App">here</div>;
+  return (
+    <div class="todos">
+      <header class="todo-header">
+        <h1>To do Applications</h1>
+        <p>Items will persist in the browser local storage</p>
+      </header>
+      <form class="form-container">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          class="input-text input-both"
+          value=""
+        />
+        <button class="input-submit">&#10010;</button>
+      </form>
+      <span class="submit-warning">Please insert your todo list</span>
+      <ul>
+        <li className="to-do-list">
+          <span>
+            <input type="checkbox" className="input-check" />
+            <span className="todo-selected">Osman Wako</span>
+          </span>
+          <button>Delete</button>
+        </li>
+        <li className="to-do-list">
+          <span>
+            <input type="checkbox" className="input-check" />
+            <span>Osman Wako</span>
+          </span>
+          <button>Delete</button>
+        </li>
+        <li className="to-do-list">
+          <span>
+            <input type="checkbox" className="input-check" />
+            <span>Osman Wako</span>
+          </span>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+  );
+  
 }
 
 export default App;
