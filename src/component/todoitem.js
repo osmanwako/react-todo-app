190ff50fe0fd426cedb4860cad3f8c2985d48439
OsmanFP <osmanwako411@gmail.com>
2023-05-10 00:00:46 +0300

    Add react icon

--- src/component/todoitem.js
+++ src/component/todoitem.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
 function Todoitem({ itemProp, setTodos, removeId }) {
     const [check, setCheck] = useState(itemProp.completed);
@@ -11,7 +12,9 @@ function Todoitem({ itemProp, setTodos, removeId }) {
             <input type="checkbox" className="input-check" checked={check} onClick={oncheck} />
             <span className={check ? 'todo-complete' : 'todo-start'}> {itemProp.title}</span>
         </span>
-        <button onClick={() => removeId(itemProp.id)}>Delete</button>
+        <button onClick={() => removeId(itemProp.id)} className="content-button">
+            <FaTrash style={{ color: "#5e5e5e", fontSize: "16px" }} />
+        </button>
     </li>;
 }
 
